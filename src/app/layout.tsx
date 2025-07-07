fc5aaa9640a827206e2a1b80cf266886d3106639
crossovka <best-login-mail@yandex.ru>
2025-07-07 17:14:44 +0900

    добавил яндекс метрику и починил стили шапки и пагинации свайпер

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ToastProvider } from '@/providers'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import { notFound } from 'next/navigation'
+import Script from 'next/script'
 
 import { Footer, Header } from '@/components/_layout'
 
@@ -61,6 +62,38 @@ export default async function RootLayout({
 
 	return (
 		<html lang="en">
+			<head>
+				{/* <!-- Yandex.Metrika counter --> */}
+				<Script id="yandex-metrika" strategy="afterInteractive">
+					{`
+					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){
+					(m[i].a=m[i].a||[]).push(arguments)};
+					m[i].l=1*new Date();
+					for (var j = 0; j < document.scripts.length; j++) {
+						if (document.scripts[j].src === r) { return; }
+					}
+					k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+					k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+					})(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+					ym(103225938, "init", {
+						clickmap:true,
+						trackLinks:true,
+						accurateTrackBounce:true,
+						webvisor:true
+					});
+				`}
+				</Script>
+				<noscript>
+					<div>
+						<img
+							src="https://mc.yandex.ru/watch/103225938"
+							style={{ position: 'absolute', left: '-9999px' }}
+							alt=""
+						/>
+					</div>
+				</noscript>
+			</head>
 			<body className={`${roboto.variable}`}>
 				<div className="wrapper" id="wrapper">
 					{headerData && (
